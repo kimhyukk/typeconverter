fad9f93ad02e12a5e421896abe61f5576e54348f
Hyuk Kim <rlagur0222@gmail.com>
2024-05-31 02:48:21 +0900

    Initial commit

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,26 @@
+package hello.typeconverter.controller;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        int intData = Integer.parseInt(data);
+        System.out.println("intData = " + intData);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
